53b663725c8672d52d2215ae672046fadd045c7e
Nicolas <nicolas.leveque@niji.com>
2019-02-07 11:07:14 +0100

    Update Manifest

---
+++ app/src/main/java/bzh/zelyon/listdetail/MainApplication.kt
@@ -0,0 +1,10 @@
+package bzh.zelyon.listdetail
+
+import android.support.multidex.MultiDexApplication
+
+class MainApplication : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
