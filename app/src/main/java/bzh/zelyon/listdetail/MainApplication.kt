8802b41ec022a4e4a6ecbe2e4ac85b71e1fa78d5
Nicolas <nicolas.leveque@niji.com>
2019-02-07 14:20:36 +0100

    Create Application

--- app/src/main/java/bzh/zelyon/listdetail/MainApplication.kt
+++ app/src/main/java/bzh/zelyon/listdetail/MainApplication.kt
@@ -1,10 +1,13 @@
 package bzh.zelyon.listdetail
 
 import android.support.multidex.MultiDexApplication
+import bzh.zelyon.listdetail.utils.DB
 
 class MainApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+
+        DB.init(this)
     }
 }
